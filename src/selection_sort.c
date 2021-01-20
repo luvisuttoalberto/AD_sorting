5d7629cdb84791ac8fe03767029dd20912470391
Alberto Luvisutto <luvisutto.alberto@gmail.com>
2021-01-20 15:19:19 +0100

    implementation selection sort

--- src/selection_sort.c
+++ src/selection_sort.c
@@ -1,8 +1,20 @@
 #include "selection_sort.h"
+#include "swap.h"
 
-void selection_sort(void *A, const unsigned int n, 
-                    const size_t elem_size, 
-                    total_order leq)
+#define ADDR(A, j, elem_size) ((A)+(j)*(elem_size))
+
+void selection_sort(void *A, const unsigned int n, const size_t elem_size, total_order leq)
 {
-    // Implement selection sort here
+    for(unsigned int i = n; i > 1; i--){
+
+        unsigned int max_j = 0;
+
+        for(unsigned int j = 1; j < i; j++){
+            if(!leq(ADDR(A, j, elem_size), ADDR(A, max_j, elem_size))){
+                max_j = j;
+            }
+        }
+
+        swap(ADDR(A, i-1, elem_size), ADDR(A, max_j, elem_size), elem_size);
+    }
 }
