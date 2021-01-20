0ee1a3266dce73d3d5fa0b5be2fcf0f15a74ac67
Alberto Luvisutto <luvisutto.alberto@gmail.com>
2021-01-20 17:44:50 +0100

    implementation heap sort

--- src/heap_sort.c
+++ src/heap_sort.c
@@ -1,8 +1,17 @@
 #include "heap_sort.h"
+#include <binheap.h>
+#include "swap.h"
 
-void heap_sort(void *A, const unsigned int n, 
-               const size_t elem_size, 
-               total_order leq)
+#define ADDR(A, j, elem_size) ((A)+(j)*(elem_size))
+
+void heap_sort(void *A, const unsigned int n, const size_t elem_size, total_order leq)
 {
-    // Implement heap sort here
+    // building a max heap
+    binheap_type * H = build_heap(A, n, n, elem_size, geq_int);
+    
+    // extracting the min (which is the maximum value, since we have build a max heap)
+    // in this way, in the end the array is already ordered
+    for(unsigned int i = 0; i < n; i++){
+        extract_min(H);
+    }
 }
