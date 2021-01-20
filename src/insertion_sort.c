6659764f8975ba07a1e8f43cfcc48ced18c4ac73
Alberto Luvisutto <luvisutto.alberto@gmail.com>
2021-01-20 12:11:20 +0100

    implementation of insertion sort

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -1,8 +1,19 @@
 #include "insertion_sort.h"
+#include "swap.h"
+
+#define ADDR(A, j, elem_size) ((A)+(j)*(elem_size))
 
 void insertion_sort(void *A, const unsigned int n, 
                     const size_t elem_size, 
                     total_order leq)
 {
-    // Implement insertion sort here
+    for(unsigned int i = 1; i < n; i++){
+
+        unsigned int j = i;
+
+        while(j > 1 && leq(ADDR(A, j, elem_size), ADDR(A, j, elem_size))){
+            swap(ADDR(A, j, elem_size), ADDR(A, j-1, elem_size), elem_size);
+            j--;
+        }
+    }
 }
