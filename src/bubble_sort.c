d8a65ee92b6d77b43e17fec37e16326992eaa84f
Alberto Luvisutto <luvisutto.alberto@gmail.com>
2021-01-20 15:04:46 +0100

    implementation bubble sort

--- src/bubble_sort.c
+++ src/bubble_sort.c
@@ -1,8 +1,15 @@
 #include "bubble_sort.h"
+#include "swap.h"
 
-void bubble_sort(void *A, const unsigned int n, 
-                 const size_t elem_size, 
-                 total_order leq)
+#define ADDR(A, j, elem_size) ((A)+(j)*(elem_size))
+
+void bubble_sort(void *A, const unsigned int n, const size_t elem_size, total_order leq)
 {
-    // Implement bubble sort here
+    for(unsigned int i = n; i > 1; i--){
+        for(unsigned int j = 0; j < i-1; j++){
+            if(!leq(ADDR(A, j, elem_size), ADDR(A, j+1, elem_size))){
+                swap(ADDR(A, j, elem_size), ADDR(A, j+1, elem_size), elem_size);
+            }
+        }
+    }
 }
