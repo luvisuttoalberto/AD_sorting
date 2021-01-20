25a703cd5daa0b128be4ad99fa3d456d0ae52285
Alberto Luvisutto <luvisutto.alberto@gmail.com>
2021-01-20 12:35:47 +0100

    debugging of insertion sort

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -3,16 +3,14 @@
 
 #define ADDR(A, j, elem_size) ((A)+(j)*(elem_size))
 
-void insertion_sort(void *A, const unsigned int n, 
-                    const size_t elem_size, 
-                    total_order leq)
+void insertion_sort(void *A, const unsigned int n, const size_t elem_size, total_order leq)
 {
     for(unsigned int i = 1; i < n; i++){
 
         unsigned int j = i;
 
-        while(j > 1 && leq(ADDR(A, j, elem_size), ADDR(A, j, elem_size))){
-            swap(ADDR(A, j, elem_size), ADDR(A, j-1, elem_size), elem_size);
+        while(j > 0 && leq(ADDR(A, j, elem_size), ADDR(A, j-1, elem_size))){
+            swap(ADDR(A, j-1, elem_size), ADDR(A, j, elem_size), elem_size);
             j--;
         }
     }
